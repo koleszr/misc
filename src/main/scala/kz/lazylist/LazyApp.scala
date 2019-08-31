9722d63bd228f8adbffd663ba1f97f420789d8e2
koleszr <koleszr.zoltan@gmail.com>
2019-08-31 08:57:16 +0200

    add ZIO && experiment composing apps with it

--- src/main/scala/kz/lazylist/LazyApp.scala
+++ src/main/scala/kz/lazylist/LazyApp.scala
@@ -1,14 +1,53 @@
 package kz.lazylist
 
+import java.io.IOException
+
 import LazyAlgorithms._
+import zio.{App, ZIO}
+import zio.console._
 
-object LazyApp {
+object LazyApp extends App {
 
-  def main(args: Array[String]): Unit = {
-    val factOfFive = fact(5)
-    println(s"5! = $factOfFive")
+  sealed trait LazyError
+  final case class IntConversionError(stringToConvert: String) extends LazyError
+  final case class IOError(e: IOException) extends LazyError
 
-    val sqrtOf9 = sqrt(9, 0.000001)
-    println(s"Square root of 9: $sqrtOf9")
+  override def run(args: List[String]): ZIO[Console, Nothing, Int] =
+    (for {
+      _ <- factorialApp
+      _ <- sqrtApp
+    } yield ()).fold(_ => 1, _ => 0)
+
+  val factorialApp: ZIO[Console, LazyError, Unit] =
+    for {
+      _            <- putStr("Enter an integer to calculate its factorial: ")
+      factorialStr <- getStrLn.mapError(IOError)
+      n            <- factorialStr.toIntZio
+      _            <- putStrLn(s"$n! = ${fact(n)}")
+    } yield ()
+
+  val sqrtApp: ZIO[Console, LazyError, Unit] =
+    for {
+      _            <- putStr("Enter a double to calculate its square root: ")
+      sqrtStr      <- getStrLn.mapError(IOError)
+      d            <- sqrtStr.toDoubleZio
+      _            <- putStr("Enter a double for tolerance between two approximations: ")
+      toleranceStr <- getStrLn.mapError(IOError)
+      tolerance    <- toleranceStr.toDoubleZio
+      _            <- putStrLn(s"Square root of $d = ${sqrt(d, tolerance)}")
+    } yield ()
+
+  implicit class StringToNumberZio[R](val str: String) extends AnyVal {
+    def toIntZio: ZIO[R, LazyError, Int] =
+      str.toIntOption match {
+        case Some(n) => ZIO.succeed(n)
+        case None    => ZIO.fail[LazyError](IntConversionError(str))
+      }
+
+    def toDoubleZio: ZIO[R, LazyError, Double] =
+      str.toDoubleOption match {
+        case Some(d) => ZIO.succeed(d)
+        case None    => ZIO.fail[LazyError](IntConversionError(str))
+      }
   }
 }
